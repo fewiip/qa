57d72bf4f14014a7af15a9091aef5770c086a2fc
Felipe Avelino <avelino.felipe@gmail.com>
2024-04-17 00:51:12 -0300

    alteracoes do dia 17 de abril

--- src/modules/Lessons/Pages/QuizPage/QuizPage.component.tsx
+++ src/modules/Lessons/Pages/QuizPage/QuizPage.component.tsx
@@ -1,34 +1,88 @@
 import styles from './QuizPage.module.css'
-import { Quiz, useLessons } from "../../api"
+import { Lesson, Quiz, useLessons } from "../../api"
 import { QuizCard } from "../../components/QuizCard/QuizCard.component"
 import { NavigationBar } from "../../../../shared/components/NavigationBar/NavigationBar.component"
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { AppLayout } from '../../../../shared/components/AppLayout';
 import { CenterCard } from '../../components/CenterCard/CenterCard.component';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../../../../shared/components/Button/Button.component';
 
 export const QuizPage = () => {
     const { quizid } = useParams()
     const { lessonid } = useParams()
-
-    const { getQuiz } = useLessons();
+    const navigate = useNavigate()
+    const { getQuiz, getLesson } = useLessons();
     const [quiz, setQuiz] = useState<Quiz>();
+    const [lesson, setLesson] = useState<Lesson>();
 
     async function fetchQuiz() {
         const response = await getQuiz(parseInt(quizid as string));
         setQuiz(response.data)
     }
+    async function fetchlesson() {
+        const response = await getLesson(parseInt(lessonid as string));
+        setLesson(response.data)
+    }
 
     useEffect(() => {
-        fetchQuiz()
+        fetchQuiz(), fetchlesson()
     }, [])
 
 
 
+    function isTheLastQuiz() {
+        if(!lesson) {
+            return false
+        }
+        let quizzesSize = lesson?.quizzes.length
+            if ((lesson?.quizzes[0]['id'] + quizzesSize) == quiz?.id) {
+                return true
+            }
+        
+        
+    }
+    function thisQuizIndex() {
+        let i = 0
+        if(!lesson) {
+            return i
+        }
+
+        let quizzesSize = lesson?.quizzes.length
+        
+        while (i < quizzesSize) {
+            if (lesson?.quizzes[i]['id'] === quiz?.id) {
+                
+                break
+            }
+            i++
+        }
+        return i
+    }
+    function nextQuizID() {
+        if (!lesson || !quiz){
+            return 0
+        }
+        let quizzesSize = lesson.quizzes.length 
+        if (quiz.id === lesson.quizzes[quizzesSize - 1]['id']) {
+            return lesson.quizzes[thisQuizIndex()]['id']
+        } else {
+            return lesson.quizzes[thisQuizIndex() + 1]['id']
+        }
+
+    }
+    async function handleNextClick() {
+        const response = await getQuiz(nextQuizID() );
+        setQuiz(response.data)
+    }
     return <AppLayout variant='grey'>
         <div className={styles.contentWrapper}>
         <CenterCard>
-            {quiz && lessonid && <QuizCard quiz={quiz} lessonID={parseInt(lessonid as string)}/>}
+            {quiz && lesson && lessonid && <QuizCard quiz={quiz}   lessonID={parseInt(lessonid as string)} key={quiz.id}/>}
+            {!isTheLastQuiz() && <div>
+            <Button style={{ padding: '16px', borderRadius: '8px', fontSize: '12px' }} onClick={handleNextClick}>Avançar</Button>
+        </div>}
         </CenterCard>
         </div>
         
--- src/modules/Lessons/components/QuizCard/QuizCard.component.tsx
+++ src/modules/Lessons/components/QuizCard/QuizCard.component.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, FunctionComponent, InputHTMLAttributes, useMemo, useState } from "react"
 import styles from './QuizCard.module.css'
-import { Quiz } from "../../api"
+import { Lesson, Quiz, useLessons } from "../../api"
 import ReactMarkdown from "react-markdown"
 import remarkBreaks from "remark-breaks";
 import professor1_happy from "../../../../assets/images/professor1_happy.png"
@@ -12,19 +12,20 @@ import { Button } from '../../../../shared/components/Button/Button.component';
 
 
 interface QuizCardProps {
-    quiz: Quiz
+    quiz: Quiz, 
     lessonID: number
 }
 
 export const QuizCard: FunctionComponent<QuizCardProps> = (props) => {
-    const { quiz } = props
+    let { quiz } = props 
     const { lessonID } = props
-    const navigate = useNavigate()
-
-    const [selectedAnswer, setSelectedAnswer] = useState()
     
-    const correctAnswer = quiz.correctAnswer;
+    const navigate = useNavigate()
+ 
+    const [selectedAnswer, setSelectedAnswer] = useState()
 
+    const correctAnswer = quiz.correctAnswer;
+ 
     const options = useMemo(() => {
         return quiz.answer.map(i => ({
             name: 'answers',
@@ -40,14 +41,14 @@ export const QuizCard: FunctionComponent<QuizCardProps> = (props) => {
 
     function handleVerify() {
         if (!selectedAnswer) return
-        toast(parseInt(selectedAnswer) === correctAnswer ?  'resposta certa': 'resposta errada')
+        toast(parseInt(selectedAnswer) === correctAnswer ? 'resposta certa' : 'resposta errada')
     }
 
     function handleEditClick() {
         navigate(`/edit/lesson/${lessonID}/quiz/${quiz?.id}`)
     }
 
-
+ 
     return <>
 
         <div className={styles.imageTextWrapper}>
@@ -66,20 +67,20 @@ export const QuizCard: FunctionComponent<QuizCardProps> = (props) => {
                     {/*<img src={graph} alt="" />*/}
                 </div>
             </div>
-        </div>
-
+        </div> 
         <div className={styles.answers}>
             <RadioGroup options={options} onChange={handleRadioChange} />
         </div>
 
         {JSON.stringify(selectedAnswer)}
         <div>
-        <Button style={{ padding: '16px', borderRadius: '8px', fontSize: '12px'}} onClick={handleVerify}>Verificar</Button>
-            
+            <Button style={{ padding: '16px', borderRadius: '8px', fontSize: '12px' }} onClick={handleVerify}>Verificar</Button>
+
         </div>
         <div>
-        <Button style={{ padding: '16px', borderRadius: '8px', fontSize: '12px'}} onClick={handleEditClick}>Editar</Button>
+            <Button style={{ padding: '16px', borderRadius: '8px', fontSize: '12px' }} onClick={handleEditClick}>Editar</Button>
         </div>
+        
 
     </>
 }
